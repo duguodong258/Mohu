e6e402bb3957048ed384138ab52622a24a30d56e
duguodong258 <dinao963>
2017-01-13 22:46:56 +0800

    提交

---
+++ app/src/main/java/com/xianrou/mohu/base/BaseFragment.java
@@ -0,0 +1,30 @@
+package com.xianrou.mohu.base;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author 咸鱼
+ * @ TIME  2017/1/13 0013.
+ * @ DES   ${TODO}.
+ */
+
+public class BaseFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
