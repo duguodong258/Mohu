b363875ac4b1db9b450a816138ff93978425fc23
duguodong258 <dinao963>
2017-01-14 09:44:47 +0800

    login finish

--- app/src/main/java/com/xianrou/mohu/activity/LoginActivity.java
+++ app/src/main/java/com/xianrou/mohu/activity/LoginActivity.java
@@ -3,6 +3,7 @@ package com.xianrou.mohu.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.xianrou.mohu.R;
 import com.xianrou.mohu.base.BaseActivity;
 
 /**
@@ -16,5 +17,6 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
     }
 }
